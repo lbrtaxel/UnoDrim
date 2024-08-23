52fd505ab39612e524bbe71a76f35bb848fae30b
lbrtaxel <axel@galadrim.fr>
2024-08-23 12:37:57 +0200

    build: setup

---
+++ api_types/card.types.ts
@@ -0,0 +1,10 @@
+export interface ICard {
+  id: number;
+  name: string;
+  description: string;
+  rarity: string;
+  type: string;
+  attack: number;
+  defense: number;
+  image: string;
+}
---
+++ vite.config.ts
@@ -0,0 +1,27 @@
+import adonisjs from "@adonisjs/vite/client";
+import react from "@vitejs/plugin-react";
+import path from "node:path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+  plugins: [
+    adonisjs({
+      /**
+       * Entrypoints of your application. Each entrypoint will
+       * result in a separate bundle.
+       */
+      entrypoints: ["frontend/src/app.tsx"],
+
+      /**
+       * Paths to watch and reload the browser on file change
+       */
+      reload: ["resources/views/**/*.edge"],
+    }),
+    react(),
+  ],
+  resolve: {
+    alias: {
+      "~": path.resolve(__dirname, "frontend/src"),
+    },
+  },
+});
